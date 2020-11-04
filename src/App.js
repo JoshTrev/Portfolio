5eb1a4a295cef3e92cab93ad487bc1307e9fb5f5
JoshTrev <joshj438t@gmail.com>
2020-11-04 13:06:04 -0500

    Copy/pasting CSS and minor html elements for styling from my previous Portfolio for consistency with the styling.

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ function App() {
   return (
     <>
       <Header
-        content={headerProp.content}
         onClick={handleButtonClick}
       />
       <main>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Footer.css";
 
-function Footer(props) {
+function Footer() {
     return (
         <>
-            <h1>{props.content}</h1>
+            <footer className="fixed-bottom">
+                <section className="row skinnyBar">
+
+                </section>
+                <section className="row row-cols-3 footerTextBlock align-items-center">
+                    <div className="col-md-4">
+
+                    </div>
+                    <div className="col-md-4 d-flex justify-content-center text-secondary">
+                        © 2020 Copyright
+                    </div>
+                    <div className="col-md-4">
+
+                    </div>
+                </section>
+            </footer>
         </>
     );
 }
