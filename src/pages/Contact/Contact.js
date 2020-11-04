b6e7f0bf4ca1e8340cd651b83bd7423be748e6a9
JoshTrev <joshj438t@gmail.com>
2020-11-04 18:25:00 -0500

    Finished adding content to Contact page, styled it too.

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,10 +1,28 @@
 import React from "react";
+import "./Contact.css";
 
 function Contact() {
     return (
         <>
-            <h1>Contact Me!</h1>
-            <p>This is my Contact Me page. Here is my contact info...</p>
+            <main className="container bg-light border rounded" id="contactContainer">
+                <form>
+                    <h1>Contact</h1>
+                    <section className="form-group">
+                        <label for="exampleFormControlInput1">Name</label>
+                        <input type="text" className="form-control" placeholder="Name" />
+                    </section>
+                    <section className="form-group">
+                        <label for="exampleFormControlInput1">Email address</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email" />
+                    </section>
+                    <section className="form-group">
+                        <label for="exampleFormControlTextarea1">Message</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" placeholder="Message"
+                            rows="3"></textarea>
+                    </section>
+                    <button type="button" id="submitButton">Submit</button>
+                </form>
+            </main>
         </>
     )
 }
