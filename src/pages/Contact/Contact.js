302b9c24278c137d8a195e5a441d3860c8304dec
JoshTrev <joshj438t@gmail.com>
2020-12-07 15:04:50 -0500

    Added functionality to contact page.

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,34 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import emailjs from 'emailjs-com';
 import "./Contact.css";
 
 function Contact(props) {
+    const [isEmailSent, setIsEmailSent] = useState({
+        emailSent: "NotSent"
+      });
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_lva146m', 'template_rsrpvsg', e.target, 'user_Ds9A3AbORSpz9ffZS6Tua')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+
+        setIsEmailSent({ emailSent: "Sent" });
+    }
+
     return (
         <>
             <main className={props.dropDownActive === "Active" ? "DropDownActiveContact container bg-light border rounded container1" : "DropDownNotActiveContact container bg-light border rounded container1"} id="contactContainer">
 
-                <form>
+                <form onSubmit={sendEmail} >
 
-                    <div id="Header2">
+                    <div id="Header2" className={isEmailSent.emailSent === "Sent" ? "Sent" : "NotSent"}>
                         <h1>Contact</h1>
                     </div>
 
-                    <section className="form-group">
+                    <section className={isEmailSent.emailSent === "Sent" ? "Sent form-group" : "NotSent form-group"} >
                         <label htmlFor="exampleFormControlInput1">Name</label>
-                        <input type="text" className="form-control" placeholder="Name" />
+                        <input type="text" className="form-control" placeholder="Name" name="name" />
                     </section>
 
-                    <section className="form-group">
+                    <section className={isEmailSent.emailSent === "Sent" ? "Sent form-group" : "NotSent form-group"} >
                         <label htmlFor="exampleFormControlInput1">Email address</label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email" />
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email" name="email" />
                     </section>
 
-                    <section className="form-group">
+                    <section className={isEmailSent.emailSent === "Sent" ? "Sent form-group" : "NotSent form-group"} >
                         <label htmlFor="exampleFormControlTextarea1">Message</label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" placeholder="Message"
-                            rows="3"></textarea>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" placeholder="Message" rows="3" name="message" ></textarea>
                     </section>
 
-                    <button type="button" id="submitButton">Submit</button>
+                    <button type="submit" id="submitButton" className={isEmailSent.emailSent === "Sent" ? "Sent" : "NotSent"} >Submit</button>
+
+                    <section className={isEmailSent.emailSent === "Sent" ? "NotSent form-group" : "Sent form-group"} >
+                        <p id="thankYouMessage" className="rounded">Thank you for your submission!</p>
+                    </section>
 
                 </form>
 
@@ -38,6 +60,7 @@ function Contact(props) {
             <br></br>
             <br></br>
             <br></br>
+
         </>
     )
 }
